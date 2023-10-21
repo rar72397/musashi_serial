eb75606bef17a27852849c1839030d30b2f4df52
Jacob Ma <89415176+Jacob-SouthernCity@users.noreply.github.com>
2023-10-20 17:58:07 -0700

    header file added

--- serial_test.c
+++ serial_test.c
@@ -1,15 +1,108 @@
-// serial test code 
+// serial test code
 
 // THINGS TO TEST
 // 1. Basic reading and writing bits to all of the buffers (Channel A and B's receive and transmit buffers)
 // 2. writing until the circular buffer is full
-// 3. 
+// 3.
 
 // declare chip as volatile global variable
 
-#include serial.c
+#include "serial.c"
 
-int main(){
-    
+// Mock buffer_init function if it's not available
+void buffer_init(buffer_t *buffer)
+{
+    // Initialize the buffer here
+    // This is a mock function, replace with actual implementation
+}
+
+// Mock buffer_write function if it's not available
+void buffer_write(buffer_t *buffer, char val)
+{
+    // Mock write to buffer
+    // This is a mock function, replace with actual implementation
+}
+
+// Mock buffer_read function if it's not available
+unsigned char buffer_read(buffer_t *buffer)
+{
+    // Mock read from buffer
+    // This is a mock function, replace with actual implementation
+    return '0'; // placeholder
+}
+
+// Mock buffer_print function if it's not available
+void buffer_print(buffer_t *buffer)
+{
+    // Mock print buffer contents
+    // This is a mock function, replace with actual implementation
+}
+
+void test_transmit_write()
+{
+    printf("Testing transmit_write function...\n");
+    // setup
+    char test_val = 'x';
+    // execute
+    transmit_write('A', test_val);
+    // assert and verify if necessary
+    // ... add your assertions and verifications here
+    printf("transmit_write function passed.\n");
+}
+
+void test_transmit_read()
+{
+    printf("Testing transmit_read function...\n");
+    // setup
+    // ... add setup code
+    // execute
+    char val = transmit_read('A');
+    // assert
+    assert(val == 'expected_value'); // replace expected_value with the actual expected value
+    printf("transmit_read function passed.\n");
+}
+
+void test_receive_write()
+{
+    printf("Testing receive_write function...\n");
+    // setup
+    char test_val = 'y';
+    // execute
+    receive_write('B', test_val);
+    // assert and verify if necessary
+    // ... add your assertions and verifications here
+    printf("receive_write function passed.\n");
+}
+
+void test_receive_read()
+{
+    printf("Testing receive_read function...\n");
+    // setup
+    // ... add setup code
+    // execute
+    char val = receive_read('B');
+    // assert
+    assert(val == 'expected_value'); // replace expected_value with the actual expected value
+    printf("receive_read function passed.\n");
+}
+
+void test_print_register()
+{
+    printf("Testing print_register function...\n");
+    // this function prints to console, so a visual check is needed
+    print_register('A', 't');
+    // check console output for correctness
+    printf("Check above output for correctness.\n");
+}
+
+int main()
+{
+    chip_init(); // if necessary for setup
+    test_transmit_write();
+    test_transmit_read();
+    test_receive_write();
+    test_receive_read();
+    test_print_register();
+    printf("All tests passed.\n");
     return 0;
 }
