a8a5321400a4def1a595ec9018568dc0879d45e2
Russell Rodriguez <rar72397@usc.edu>
2023-10-20 17:30:58 -0700

    making test file

---
+++ serial_test.c
@@ -0,0 +1,15 @@
+// serial test code 
+
+// THINGS TO TEST
+// 1. Basic reading and writing bits to all of the buffers (Channel A and B's receive and transmit buffers)
+// 2. writing until the circular buffer is full
+// 3. 
+
+// declare chip as volatile global variable
+
+#include serial.c
+
+int main(){
+    
+    return 0;
+}
